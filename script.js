f4aa1d0368a38b2e9848ff55c36d53ae92d6e0cc
Asim Sapkota <asimsapkota1836@gmail.com>
2021-12-25 01:28:12 +0545

    scheduler function added with left-rotate function as utility.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const BLOCKLENGTH = 512;
 const MESSAGELENGTH = 64;
 
+// ------ UTILITY ----------//
+
+function leftRotate(ary, shift) {
+  for (let i = 0; i < shift; i++) {
+    let tmpStore = ary.shift();
+    ary.push(tmpStore);
+  }
+  return ary;
+}
+
+console.log(leftRotate([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
+
+// ------ UTILITY ----------//
+
 function toBinary(str) {
   /// convert string to its ascii based binary form
   /// parameter: string
@@ -63,3 +77,34 @@ console.log(
   createBlock("abcdefghijklmnopqrstuvwxyz"),
   createBlock("abcdefghijklmnopqrstuvwxyz").length
 );
+
+function messageScheduler(ary) {
+  let scheduledChunkReq = 80;
+  let initchunkReq = 16;
+  let bitSize = 32;
+  let messageChunks = [];
+
+  for (let i = 0; i < scheduledChunkReq; i++) {
+    if (i < initchunkReq) {
+      messageChunks.push(ary.slice(i * bitSize, (i + 1) * bitSize));
+    } else {
+      scheduler(i, messageChunks);
+    }
+  }
+
+  return messageChunks;
+}
+
+function scheduler(i, ary) {
+  let bitSize = 32;
+  let tmpAry = [];
+  for (let k = 0; k < bitSize; k++) {
+    tmpAry.push(
+      ary[i - 3][k] ^ ary[i - 8][k] ^ ary[i - 14][k] ^ ary[i - 16][k]
+    );
+  }
+  leftRotate(tmpAry, 1);
+  ary.push(tmpAry);
+}
+
+console.log(messageScheduler(createBlock("abc")));
