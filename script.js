ef7b3dd5e5157473c46919951c1040ddca39648d
Asim Sapkota <asimsapkota1836@gmail.com>
2021-12-25 13:41:10 +0545

    bitwise and, or and not functions for array of variable lengths and hex to binary converter functions created for utility.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const BLOCKLENGTH = 512;
 const MESSAGELENGTH = 64;
 
+let h0 = "0x67452301";
+let h1 = "0xEFCDAB89";
+let h2 = "0x98BADCFE";
+let h3 = "0x10325476";
+let h4 = "0xC3D2E1F0";
+
 // ------ UTILITY ----------//
 
 function leftRotate(ary, shift) {
@@ -11,7 +17,121 @@ function leftRotate(ary, shift) {
   return ary;
 }
 
-console.log(leftRotate([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
+// console.log(leftRotate([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
+
+function bitNot(ary) {
+  let tmpAry = [];
+
+  for (let value of ary) {
+    value !== 0 ? tmpAry.push(0) : tmpAry.push(1);
+  }
+
+  return tmpAry;
+}
+
+// console.log(bitNot([1, 1, 1, 0, 0, 0, 1, 0, 1]));
+
+function bitAnd(ary0, ary1) {
+  let tmpAry = [];
+  let loopTo = ary0.length <= ary1.length ? ary0.length : ary1.length;
+
+  for (let i = 0; i < loopTo; i++) {
+    tmpAry.push(ary0[i] & ary1[i]);
+  }
+
+  return tmpAry;
+}
+
+// console.log(bitAnd([0, 1, 1, 1, 0, 0, 1, 0], [0, 1, 1, 0, 0, 0, 1]));
+
+function bitOr(ary0, ary1) {
+  let tmpAry = [];
+  let loopTo = ary0.length <= ary1.length ? ary0.length : ary1.length;
+
+  for (let i = 0; i < loopTo; i++) {
+    tmpAry.push(ary0[i] | ary1[i]);
+  }
+
+  return tmpAry;
+}
+
+// console.log(bitOr([0, 1, 1, 1, 0, 0, 1, 0], [0, 1, 1, 0, 0, 0, 1]));
+
+function bitXor(ary0, ary1) {
+  let tmpAry = [];
+  let loopTo = ary0.length <= ary1.length ? ary0.length : ary1.length;
+
+  for (let i = 0; i < loopTo; i++) {
+    tmpAry.push(ary0[i] ^ ary1[i]);
+  }
+
+  return tmpAry;
+}
+
+// console.log(bitXor([0, 1, 1, 1, 0, 0, 1, 0], [0, 1, 1, 0, 0, 0, 1]));
+
+function hexToBin(hex) {
+  hex = hex.replace("0x", "").toLowerCase();
+  let tmpAry = [];
+
+  for (let char of hex) {
+    switch (char) {
+      case "0":
+        tmpAry = tmpAry.concat([0, 0, 0, 0]);
+        break;
+      case "1":
+        tmpAry = tmpAry.concat([0, 0, 0, 1]);
+        break;
+      case "2":
+        tmpAry = tmpAry.concat([0, 0, 1, 0]);
+        break;
+      case "3":
+        tmpAry = tmpAry.concat([0, 0, 1, 1]);
+        break;
+      case "4":
+        tmpAry = tmpAry.concat([0, 1, 0, 0]);
+        break;
+      case "5":
+        tmpAry = tmpAry.concat([0, 1, 0, 1]);
+        break;
+      case "6":
+        tmpAry = tmpAry.concat([0, 1, 1, 0]);
+        break;
+      case "7":
+        tmpAry = tmpAry.concat([0, 1, 1, 1]);
+        break;
+      case "8":
+        tmpAry = tmpAry.concat([1, 0, 0, 0]);
+        break;
+      case "9":
+        tmpAry = tmpAry.concat([1, 0, 0, 1]);
+        break;
+      case "a":
+        tmpAry = tmpAry.concat([1, 0, 1, 0]);
+        break;
+      case "b":
+        tmpAry = tmpAry.concat([1, 0, 1, 1]);
+        break;
+      case "c":
+        tmpAry = tmpAry.concat([1, 1, 0, 0]);
+        break;
+      case "d":
+        tmpAry = tmpAry.concat([1, 1, 0, 1]);
+        break;
+      case "e":
+        tmpAry = tmpAry.concat([1, 1, 1, 0]);
+        break;
+      case "f":
+        tmpAry = tmpAry.concat([1, 1, 1, 1]);
+        break;
+      default:
+        return [];
+    }
+  }
+  return tmpAry;
+}
+
+// console.log(hexToBin(h0));
 
 // ------ UTILITY ----------//
 
